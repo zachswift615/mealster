d964d99e0bcdaf31386e658fa09630e6d298ed98
Zach Swift <zach.swift@decisionsource.com>
2018-02-13 19:54:40 -0600

    Starting point for meal models

--- mealsdb/mealster_api/api/admin.py
+++ mealsdb/mealster_api/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import UnitOfMeasure, MealItem, Meal, Ingredient
 # Register your models here.
+admin.site.register(UnitOfMeasure)
+admin.site.register(MealItem)
+admin.site.register(Meal)
+admin.site.register(Ingredient)
+
--- mealsdb/mealster_api/api/models.py
+++ mealsdb/mealster_api/api/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UnitOfMeasure(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    unit_of_measure = models.ForeignKey(
+        UnitOfMeasure, on_delete=models.CASCADE,
+        blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class MealItem(models.Model):
+    name = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    ingredients = models.ManyToManyField(Ingredient)
+
+    def __str__(self):
+        return self.name
+
+
+class Meal(models.Model):
+    name = models.CharField(max_length=255)
+    serves = models.IntegerField(blank=True, null=True)
+    meal_items = models.ManyToManyField(MealItem)
+
+    def __str__(self):
+        return self.name
+
+
